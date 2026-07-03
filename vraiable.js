5046993e8bebca927e940c38fafd1b2650409cd4
C-Null-S-Error-T <habibcs17r@gmail.com>
2026-07-03 06:44:21 +0600

    varable-1

---
+++ vraiable.js
@@ -0,0 +1,16 @@
+/*VARIABLE ==> VARY + ABLE
+*5 things to declare a variable*/
+// 1.var name = "John"; // can be redeclared and updated
+// 2.let name = "John"; // can be updated but not redeclared
+// 3.const name = "John"; // cannot be updated or redeclared
+// 4.var name; // can be declared without initialization
+// 5.let name; // can be declared without initialization but cannot be redeclared
+var highat = 5;
+var many = 500;
+var distense = 1000;
+var price = 70;
+
+var age;
+console.log(age); // undefined
+console.log(highat);
+console.log(many);
