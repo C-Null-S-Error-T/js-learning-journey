241c6b7dc9f621772f89495777bc642ff4b15eb3
C-Null-S-Error-T <habibcs17r@gmail.com>
2026-07-05 05:58:23 +0600

    if else or else if

---
+++ conditionla/ifcondtion.js
@@ -0,0 +1,15 @@
+/*if else statement*/
+/*write a if else statement away
+if(condition){
+  statement
+}
+  else{
+    statement
+  }*/
+ var time =8;
+ if(time > 7){
+    console.loge("a lag boy in te work in my self");
+ }
+ else{
+    console.log("a leagye boy in te work in my self");
+ }
---
+++ conditionla/nextted.js
@@ -0,0 +1,34 @@
+//node .\conditionla\nextted.js this is th comand
+// else if statement
+// const Ashoop = 500;
+// if(Ashoop<1000){
+//     console.log("we are shop in a this hope");
+// }
+// else if(Ashoop<2000){
+//     console.log("we are shop in a this hope");
+// }
+// else if(Ashoop<3000){
+//     console.log("we are shop in a this hope");
+// }
+// else{
+//     console.log("we are not shop in a this hope");
+// }
+// nested if else statement
+//------------------------------------
+const age = 15
+if (age >18) {
+    console.log("you are going to class 12th");
+}
+else {
+    if (age > 15) {
+        console.log("you are going to class 10th");
+    }
+    else {
+        if (age > 10) {
+            console.log("your going to class 8th");
+        }
+        else {
+            console.log("you are going to class 5th");
+        }
+    }
+}
