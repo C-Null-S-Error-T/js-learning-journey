d1e9f0bc5f6a3da7f3159e0fb544175f68a961ca
C-Null-S-Error-T <habibcs17r@gmail.com>
2026-07-04 06:16:00 +0600

    conditon name

---
+++ conditionla/intodrockton_condition.js
@@ -0,0 +1,12 @@
+/* # COMPARISON OPERATORS
+1. BIGGER :>
+2. LESS :<
+3. EQUAL :==
+4. NOT EQUAL :!=
+5. GREATER OR EQUAL :>=
+6. LESS OR EQUAL :<=
+ **************************
+ 7.AND :&&
+ 8.OR :||
+ 9.NOT :!
+*/
