59e3477a9e4aa522a7ce545e95a349b2d6c70e7e
C-Null-S-Error-T <habibcs17r@gmail.com>
2026-07-04 07:14:55 +0600

    some difrent logick

---
+++ conditionla/comparison.js
@@ -0,0 +1,12 @@
+// node .\conditionla\comparison.js this my round code
+// console.log(4 > 3);
+// console.log(4 < 3);
+// console.log(4 != 3);
+// console.log(4 >= 3);
+// console.log(4 <= 3);
+// console.log(4 <= 3);
+console.log(33 == "33"); //he no check type value because it result is true
+console.log(33 === "33"); //he check type and value because it result is false
+
+console.log(33 != "33"); //he no check type value because it result is false
+console.log(33 !== "33"); //he check type and value because it result is true
