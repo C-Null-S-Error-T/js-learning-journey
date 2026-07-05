8e9259fefb50cbd427a8a6895432e89fadebdab2
C-Null-S-Error-T <habibcs17r@gmail.com>
2026-07-05 07:04:49 +0600

    ternary or string

---
+++ conditionla/ternary.js
@@ -0,0 +1,14 @@
+//ternary operator
+// const age = 20;
+// const result = age >= 18 ? "You are an adult" : "You are not an adult";
+// console.log(result);
+// let number = 10;
+// const justes = false;
+// justes === false ? number+=20 : number=0;
+// console.log(number);
+let prodect = 50;
+const track= true;
+track===true?prodect+=50:prodect=0;
+console.log(prodect);
+
+//note empty string is false and any string is true
