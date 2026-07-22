499a03d76a78f78b37a6af900851c569bf54a95d
C-Null-S-Error-T <habibcs17r@gmail.com>
2026-07-22 20:54:03 +0600

    the loop event nambur

---
+++ loop_in_jabascript/loop.js
@@ -0,0 +1,14 @@
+// console.log("THIS LOOP SHOW IS A IVEN NAUMBER")
+// for (let l = 0; l < 20; l++){
+//     if(l%2==0)
+//     {
+//         console.log(l)
+//     }
+// }
+// for (const i = 1; i < 20; i += 2) {
+//     console.log(i)
+// }
+// const is not working because const not chang the ballo 
+for (let i = 1; i < 20; i += 2) {
+    console.log(i)
+}
