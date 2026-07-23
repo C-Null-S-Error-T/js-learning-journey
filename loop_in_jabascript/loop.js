e8d29cdcd5573a7d867021f2e90dd4d1e4915d38
C-Null-S-Error-T <habibcs17r@gmail.com>
2026-07-23 21:39:38 +0600

    break stetmant

--- loop_in_jabascript/loop.js
+++ loop_in_jabascript/loop.js
@@ -9,6 +9,25 @@
 //     console.log(i)
 // }
 // const is not working because const not chang the ballo 
-for (let i = 1; i < 20; i += 2) {
-    console.log(i)
+// for (let i = 1; i < 20; i += 2) {
+//     console.log(i)
+// }
+// use the break stetment and pricktis
+// let cad = 55; let num = 0;
+// while (true) {
+
+//     if (num == cad) break;
+//     num += 1;
+//     console.log(num);
+// }
+
+//problem find the umber in then big one;
+const numbers = [12, 25, 40, 51, 70, 90];
+const num1 = 69;
+let i=0;
+while (i<numbers.length) {
+    if (num1 <= numbers[i]){console.log(numbers[i]);
+        break;} 
+    i++;
 }
+
