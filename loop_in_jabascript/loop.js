32dfc58fa870b7125b62e07bd9f825db24087396
C-Null-S-Error-T <habibcs17r@gmail.com>
2026-07-24 12:51:04 +0600

    code commit

--- loop_in_jabascript/loop.js
+++ loop_in_jabascript/loop.js
@@ -22,12 +22,44 @@
 // }
 
 //problem find the umber in then big one;
-const numbers = [12, 25, 40, 51, 70, 90];
-const num1 = 69;
-let i=0;
-while (i<numbers.length) {
-    if (num1 <= numbers[i]){console.log(numbers[i]);
-        break;} 
-    i++;
-}
+// const numbers = [12, 25, 40, 51, 70, 90];
+// const num1 = 69;
+// let i = 0;
+// while (i < numbers.length) {
+//     if (num1 <= numbers[i]) {
+//         console.log(numbers[i]);
+//         break;
+//     }
+//     i++;
+// }
 
+
+//------------------------------------------
+// the main memoribul pont in the loop is prefix / postfix ++i or i++;
+
+// console.log("this is postfix in the loop");
+// let i = 0;
+// while (i++ < 5)
+// {
+//     console.log(i);
+
+// }
+// console.log("this is prefix in the loop");
+// let j = 0;
+// while (++j < 5) {
+
+//     console.log(j);
+// }
+
+let vn = 10;
+for (let i = 2; i <= vn; i++) {
+    let count = 0;
+    console.log(`i = ${i}`);
+    for (let j = 2; j <= i; j++) {
+        console.log(`j = ${j}`);
+        if (i % j == 0) ++count;
+        console.log(`count =${count}`);
+        if (count > 1) break;
+    }
+    if (count == 1) console.log(i);
+}
