a6e8e770362400d24589f78c187cb1a80b32c88e
C-Null-S-Error-T <habibcs17r@gmail.com>
2026-09-08 10:02:34 +0600

    strin

---
+++ array/inorout.js
@@ -0,0 +1,9 @@
+//Method push/pop -- Shift/Unshift
+// let array = [10,20,30,40.50,60,70,80,90,100];
+// console.log(array);
+// //This is the Push in element in array;
+// array.push(233);
+// array.push(453);
+// console.log(array);
+let array = ['Red,blue,orange,grin,yeall,skyboue'];
+console.log(array);
---
+++ strings_object/strin.js
@@ -0,0 +1,18 @@
+//node .\conditionla\comparison.js
+const country = "Bangladesh";
+const district = `Dhaka`; //this string is a very powerful and flexible way.
+console.log(country);
+console.log(district);
+// a use of spaceil perpaze in the dickler in some cases
+const division = new String('sylt');
+console.log(typeof division);
+console.log(division);
+// a memorable string lent mingth a whitch box or gor you use 
+// another way say to a string witch memory spec is youse;
+
+
+console.log("string is immutable in nature. once you create a string you can not change it. but you can create a new string based on the old one.");
+
+console.log("-------------------------------------------------");
+
+console.log("array is mutable in nature. you can change the value of an array after it is created. but you can not change the length of an array after it is created.");
